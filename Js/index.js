26c23a25be55b7fde600b1a2e3f0b516dbf73440
Pierret_Fabrino <rakotoharilalainafabrino@gmail.com>
2023-03-11 09:26:14 +0100

    mofication

---
+++ Js/index.js
@@ -0,0 +1,45 @@
+$(document).ready(() => {
+
+
+    $(".btn-plus").click(function () {
+        $("div").animate({
+            height: 'toggle'
+        });
+    });
+
+    $("#ok").on('click', () => {
+        back()
+    })
+
+})
+
+const preparData = () => {
+    let email = $('#email').val()
+    let pwd = $('#pwd').val()
+    let msg = ""
+
+    if (!email || !pwd) {
+        msg = "Tous les devront etre remplis"
+    } else {
+        msg = "Votre email : " + email + " ,votre mdp : " + pwd
+    }
+    let msg_box = $('#msg')
+    msg_box.text(msg)
+
+    // chagement dans html pour un seul valeur a modifier dans css
+    $('#alert-box').css("display", "flex")
+
+    // changement dans html pour des plusieur valeur a modifier dans css
+
+    // $('#alert-box').css({
+    //     display : "flex",
+    //     height : "val"
+    // })
+
+
+}
+
+const back = () => {
+    $('#alert-box').css("display", "none")
+}
+
